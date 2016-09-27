7049a0a36feaa6c3dc1a9566b4b08adbf0b1114c
Brian Grant and Sebastian Prokuski <brian@>
2016-09-27 11:28:20 -0700

    install ability to return api request from youtube data api

---
+++ app/models/video.rb
@@ -0,0 +1,18 @@
+class Video < ApplicationRecord
+  before_create :get_info
+
+  private
+  def get_info
+    begin
+      response = JSON.parse(RestClient::Request.new(
+      method: :get,
+      url: "https://www.googleapis.com/youtube/v3/videos",
+      headers: {params: { key: 'AIzaSyCDUUqDeIAy9_zVMugOIwzA1mgyb4nAGWM', id: '7LnBvuzjpr4', part: 'contentDetails'}}
+      ).execute)
+    rescue RestClient::BadRequest => error
+      message = JSON.parse(error.response)['message']
+      errors.add(:base, message)
+      throw(:abort)
+    end
+  end
+end
